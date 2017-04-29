8e4d0f44a70c25aee1c91711b5726313127fbf65
Ayumi Saito <ayumisaito27@gmail.com>
2017-04-29 15:21:09 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,144 +1,131 @@
-// array for button color 
-// var button = [red,blue,green,yellow];
-
-// add sound 
+/* ===== sound info ===== */
 var url = "https://s3.amazonaws.com/freecodecamp/";
 var audio =["simonSound1.mp3","simonSound2.mp3","simonSound3.mp3","simonSound4.mp3"];
 
-// array
-//show round , up to 20
-var round = [];
+
+/* ===== array ===== */
+// store information. random number.player number.
 var random = [];
-//store random pad number
-var num = [];
 var playerNum = [];
 
-//game start 
+
+/* ===== game start ===== */
 $(document).ready(function(){
 $("#start").on("click",function(){
   count = 0;
   random = [];
   playerNum = [];
-  startGame();
-  
+  gameRound();
  }); 
-   }); 
+}); 
 
-// STRT GAME FUNCTION 
-
- function startGame(){
-  var i = Math.floor((Math.random()*4)+1);
-  random.push(i);
+/* ===== game round ===== */
+function gameRound(){
+  playerNum = [];
   count++;
-  if (count < 21){
-    $("#count").html("" +count);
-    randomHighlight();
+  if (count < 10){
+  $("#count").html("" +count);
+  randomNum();
   } else {
-    // game finish! 
-    $("#count").html("#");
+  // game finish! 
+  $("#count").html("#");
+  alert("finish!");
   }
 }
 
-// Function randomHighlight : Light up to random sequence in the random array 
-
-function randomHighlight(){
-  for (var i = 0; i < random.length; i++){
-    // checkTimeout(i);
-    checkTimeout(i);
+function gameContinue(){
+  playerNum = [];
+  if (count < 10){
+  $("#count").html("" +count);
+  randomNum();
+  } else {
+  // game finish! 
+  $("#count").html("#");
+  alert("finish!");
   }
 }
 
-// Function checkTimeout: setup Time 
+/* ===== select random number ===== */
+
+function randomNum(){
+   var i = Math.floor((Math.random()*4)+1);
+   random.push(i);
+   for (var i = 0; i < random.length; i++){
+   checkTimeout(i);
+}
+}
+
+/* ===== highlight ===== */
+
 function checkTimeout(i){
   setTimeout(function(){
   highlight(random[i]);
   }, 1000 * i + 1000);
+  
 }
 
 function highlight(i){
    $(".slice" + i).css("opacity", "0.5");
     setTimeout(function(){
     $(".slice" + i).css("opacity", "1.0");
-    },3000);
-    new Audio(url + audio[i]).play(); 
-    console.log("ran: " + ".slice" + i);
-    console.log("random");
-    console.log("playerNum");
-  
+    },1000);
+    new Audio(url + audio[i-1]).play();
 }
 
-//Function playerMove : 
-
-
+/* ===== player move ===== */
 $(".slice1").on("click",function(){
-  playerNum.push(1);
-  $(".slice1").css("opacity", "0.5");
+    playerNum.push(1);
     setTimeout(function(){
-    $("..slice1").css("opacity", "1.0");
-    },200);
-    new Audio(url + audio[1]).play(); 
-  alert(playerNum);
+      highlight(1);
+    }, 500);
+    checkColor();
  }); 
- 
- $(".slice2").on("click",function(){
-  playerNum.push(2);
-   checkTimeout(i);
-  alert(playerNum);
+
+$(".slice2").on("click",function(){
+    playerNum.push(2);
+    checkTimeout(2);
+    setTimeout(function(){
+      highlight(2);
+    }, 500);
+     checkColor();
  }); 
-  
-  $(".slice3").on("click",function(){
-  playerNum.push(3);
-    checkTimeout(i);
-  alert(playerNum);
+
+$(".slice3").on("click",function(){
+    playerNum.push(3);
+    checkTimeout(3);
+      setTimeout(function(){
+      highlight(3);
+    }, 500);
+     checkColor();
  }); 
-  
-  $(".slice4").on("click",function(){
-  playerNum.push(4);
-    checkTimeout(i);
-  alert(playerNum);
+
+$(".slice4").on("click",function(){
+    playerNum.push(4);
+    checkTimeout(4);
+      setTimeout(function(){
+      highlight(4);
+    }, 500);
+     checkColor();
  }); 
-  
-  console.log(random);
-  console.log(playerNum);
-  startGame();
 
 
-// function checkColor: check if random arr and player arr matches 
-/* function checkColor(arr1,arr2){
-  if(arr1[i] === arr2[i]){
-    startGame();
-  } else {
-    alert("noo");
+/* ===== check number/color ===== */
+function checkColor(){
+  for(var i=0; i<random.length; i++){
+    if(random[i] === playerNum[i]){
+      console.log("check random/player");
+      console.log(random[i]);
+      console.log(playerNum[i]);
+      console.log("random arr" + "" + random);
+      console.log("player arr" + "" + playerNum);
+      console.log("count" + "" + count);
+      setTimeout(function(){
+         gameRound();
+      },3000);
+
+    } else {
+      console.log("not possible");
+      
+    }
   }
 }
-
-/*function playerMove(){
-  $("li").click(function(e) {
-    e.preventDefault();
-    if(!$(this).hasClass("slice1")){
-     alert("slice1");
-   }
-  })
-  startGame();
-};  
-
-/*var button = $("li");
-
-button.click(function(e){
-  e.preventDefault();
-  alert("2");
-  if($(this).attr("class") == "slice1")
-    alert("1");
-})
-
-/*function playerMove(){
-  $("button").click(function(e) {
-    e.preventDefault();
-    if(!$(this).hasClass("red")){
-     alert("slice1");
-   }
-  })
-  startGame();
-};  
-*/
-  
